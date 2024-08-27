7c6c6a5d0b4bb00a484e3a6b1d94842821c0b6ff
Piotr S <177131800+pedrosmake@users.noreply.github.com>
2024-08-27 14:09:41 +0200

    US037: Use API_URL env var to know where is backend

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -1,8 +1,13 @@
 import axios, { AxiosResponse } from "axios";
+import { API_URL } from "../config";
+
+const axiosInstance = axios.create({
+    baseURL: API_URL
+});
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-        const response: AxiosResponse = await axios.get("http://localhost:8080/api/test");
+        const response: AxiosResponse = await axiosInstance.get("/api/test");
 
         return response.data;
     } catch (e) {
