cd08f97f714eb69b938767d490bdc89865129e98
“Aleksandra” <aleksandramordzon@gmail.com>
2024-08-28 10:56:52 +0200

    US038: changed linter rules and fixed linter errors

--- src/services/TestService.ts
+++ src/services/TestService.ts
@@ -1,17 +1,17 @@
-import axios, { AxiosResponse } from "axios";
-import { API_URL } from "../config";
+import axios, { AxiosResponse } from 'axios';
+import { API_URL } from '../config';
 
 const axiosInstance = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
 });
 
 export const getDatabases = async (): Promise<string[]> => {
     try {
-        const response: AxiosResponse = await axiosInstance.get("/api/test");
+        const response: AxiosResponse = await axiosInstance.get('/api/test');
 
         return response.data;
     } catch (e) {
         console.log(e);
         throw new Error('Failed to get databases');
     }
-}
+};
