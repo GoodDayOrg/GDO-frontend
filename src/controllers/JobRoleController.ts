fe002384b616ab3faf7ceda03df805c146290034
PiotrM <128514914+PiotrMichalak1@users.noreply.github.com>
2024-09-03 12:30:50 +0200

    Differentiated between getAllJob and getJobFiltered

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -1,15 +1,15 @@
 import express from 'express';
-import { getAllJobRoles } from '../services/JobRoleService';
+import { getFilteredJobRoles , getAllJobRoles } from '../services/JobRoleService';
 import { JobRoleFilterParams } from '../models/JobRoleFilterParams';
 import { extractJobRoleFilterParams } from '../utils/JobRoleUtil';
 
-export const getJobRoles = async (
+export const getJobRolesFiltered = async (
   req: express.Request,
   res: express.Response,
 ): Promise<void> => {
   try {
     const filters: JobRoleFilterParams = extractJobRoleFilterParams(req);
-    const jobRoles = await getAllJobRoles(filters);
+    const jobRoles = await getFilteredJobRoles(filters);
     
     res.render('job-role-list', { jobRoles });
   } catch (e) {
@@ -17,3 +17,19 @@ export const getJobRoles = async (
     res.render('job-role-list');
   }
 };
+
+export const getJobRoles = async (
+  req: express.Request,
+  res: express.Response,
+): Promise<void> => {
+  try {
+    const jobRoles = await getAllJobRoles();
+    
+    res.render('job-role-list', { jobRoles });
+  } catch (e) {
+    res.locals.errormessage = e.message;
+    res.render('job-role-list');
+  }
+};
+
+
--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -4,11 +4,20 @@ import { axiosInstance } from '../config';
 import { JobRoleFilterParams } from '../models/JobRoleFilterParams';
 import qs from 'qs';
 
-export const getAllJobRoles = async (filters?: JobRoleFilterParams): Promise<JobRoleResponse[]> => {
+export const getFilteredJobRoles = async (filters?: JobRoleFilterParams): Promise<JobRoleResponse[]> => {
   try {
     const response: AxiosResponse = await axiosInstance.get('/api/job-roles', {
       params: filters
     });
+    return response.data;
+  } catch (e) {
+    throw new Error('Currently no job-roles available');
+  }
+};
+
+export const getAllJobRoles = async (): Promise<JobRoleResponse[]> => {
+  try {
+    const response: AxiosResponse = await axiosInstance.get('/api/job-roles');
     return response.data;
   } catch (e) {
     throw new Error('Currently no job-roles available');
