7083659355e059abdd54616c27259d05d4bd7865
PiotrM <128514914+PiotrMichalak1@users.noreply.github.com>
2024-09-03 11:50:17 +0200

    Added correct data to checkboxes

--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -6,11 +6,6 @@ import qs from 'qs';
 
 export const getAllJobRoles = async (filters?: JobRoleFilterParams): Promise<JobRoleResponse[]> => {
   try {
-    const queryString = qs.stringify(filters, { skipNulls: true, indices: true });
-    const fullUrl = `/api/job-roles?${queryString}`;
-
-    // Log the full URL
-    console.log('Request URL:', fullUrl);
     const response: AxiosResponse = await axiosInstance.get('/api/job-roles', {
       params: filters
     });
