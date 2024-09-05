53c6ff694ce4bf2d5a1f072d082790d3ccf05eb7
PiotrM <128514914+PiotrMichalak1@users.noreply.github.com>
2024-09-05 13:01:59 +0200

    Edited service tests for filtering

--- test/unit/services/JobRoleServiceTests.ts
+++ test/unit/services/JobRoleServiceTests.ts
@@ -1,8 +1,12 @@
 import MockAdapter from 'axios-mock-adapter';
 import { expect } from 'chai';
 import { JobRoleResponse } from '../../../src/models/JobRoleResponse';
-import { getAllJobRoles } from '../../../src/services/JobRoleService';
+import {
+  getAllJobRoles,
+  getFilteredJobRoles,
+} from '../../../src/services/JobRoleService';
 import { axiosInstance } from '../../../src/config';
+import { JobRoleFilterParams } from '../../../src/models/JobRoleFilterParams';
 
 const URL: string = '/api/job-roles';
 
@@ -47,6 +51,48 @@ describe('JobRoleService', function () {
 
       try {
         await getAllJobRoles();
+        expect(true).equal(false);
+      } catch (e) {
+        expect(e.message).to.equal('Currently no job-roles available');
+        return;
+      }
+    });
+  });
+
+  describe('getFilteredJobRoles', function () {
+    it('should return filtered job roles from response', async () => {
+      const data = [jobRoleResponse];
+
+      mock.onGet(URL + '/filter').reply(200, data);
+
+      const filterParams: JobRoleFilterParams = {
+        roleName: 'engineer',
+        jobRoleLocation: ['Gdansk', 'Buenos Aires'],
+      };
+
+      const results = await getFilteredJobRoles(filterParams);
+      results[0].closingDate = new Date(results[0].closingDate);
+
+      expect(results[0]).to.deep.equal(jobRoleResponse);
+    });
+
+    it('should throw exception when 500 error returned from axios', async () => {
+      mock.onGet(URL + '/filter').reply(500);
+
+      try {
+        await getFilteredJobRoles();
+        expect(true).equal(false);
+      } catch (e) {
+        expect(e.message).to.equal('Currently no job-roles available');
+        return;
+      }
+    });
+
+    it('should throw exception when 404 error returned from axios', async () => {
+      mock.onGet(URL + '/filter').reply(404);
+
+      try {
+        await getFilteredJobRoles();
         expect(true).equal(false);
       } catch (e) {
         expect(e.message).to.equal('Currently no job-roles available');
