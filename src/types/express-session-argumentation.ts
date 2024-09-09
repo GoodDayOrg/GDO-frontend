7130fa696bac7476e12f84623fc6259e109ee25e
Piotr S <177131800+pedrosmake@users.noreply.github.com>
2024-09-09 16:55:51 +0200

    US028: express-session with filteredJobRoles

--- src/types/express-session-argumentation.ts
+++ src/types/express-session-argumentation.ts
@@ -1,7 +1,9 @@
 import 'express-session';
+import { JobRoleResponse } from '../models/JobRoleResponse';
 
 declare module 'express-session' {
   interface SessionData {
     token: string;
+    filteredJobRoles: JobRoleResponse[];
   }
 }
