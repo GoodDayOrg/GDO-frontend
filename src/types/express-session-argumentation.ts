c533f88edd265dbf15d71b3fc6d29beb7868a3fb
Piotr S <177131800+pedrosmake@users.noreply.github.com>
2024-09-10 11:23:54 +0200

    US028: keep filters in session

--- src/types/express-session-argumentation.ts
+++ src/types/express-session-argumentation.ts
@@ -1,9 +1,11 @@
 import 'express-session';
 import { JobRoleResponse } from '../models/JobRoleResponse';
+import { JobRoleFilterParams } from '../models/JobRoleFilterParams';
 
 declare module 'express-session' {
   interface SessionData {
     token: string;
     jobRoles: JobRoleResponse[];
+    filters: JobRoleFilterParams;
   }
 }
